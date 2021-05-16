062d8da28500fd3f4384d501d2faffc180f34cae
Arman <armanmkrtchyan@tidepoollabs.com>
2021-05-16 19:35:41 +0400

    Create migration for store and store product tables

---
+++ migrations/m210516_151251_create_store_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%store}}`.
+ */
+class m210516_151251_create_store_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%store}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string('150'),
+        ]);
+
+        for ($i = 1; $i < 4; $i++) {
+            $this->insert('store', [
+                'title' => 'Store ' . $i
+            ]);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%store}}');
+    }
+}
---
+++ migrations/m210516_151822_create_store_product_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%store_product}}`.
+ */
+class m210516_151822_create_store_product_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%store_product}}', [
+            'id' => $this->primaryKey(),
+            'store_id' => $this->integer()->notNull(),
+            'upc' => $this->string(50),
+            'title' => $this->string(150),
+            'price' => $this->integer(),
+        ]);
+
+        $this->createIndex(
+            'idx-store_product-store_id',
+            'store_product',
+            'store_id'
+        );
+
+        $this->addForeignKey(
+            'fk-store_product-store_id',
+            'store_product',
+            'store_id',
+            'store',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey(
+            'fk-store_product-store_id',
+            'store_product'
+        );
+
+        $this->dropIndex(
+            'store_product-store_id',
+            'store_product'
+        );
+
+        $this->dropTable('{{%store_product}}');
+    }
+}
